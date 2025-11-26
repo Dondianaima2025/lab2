0a51649ba08f51c5fb20afb49618e9471990dd48
Dondianaima2025 <pauldondianaima@gmail.com>
2025-11-26 16:40:00 -0500

    Update MainActivity.kt

--- app/src/main/java/com/example/nanou/MainActivity.kt
+++ app/src/main/java/com/example/nanou/MainActivity.kt
@@ -45,10 +45,7 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity2::class.java)
             resultLauncher.launch(intent)
         }
-        questionText.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
-        }
+      
         questionText.setOnClickListener {
             questionText.visibility = View.INVISIBLE
             answerText.visibility = View.VISIBLE
@@ -58,15 +55,6 @@ class MainActivity : AppCompatActivity() {
             questionText.visibility = View.VISIBLE
             answerText.visibility = View.INVISIBLE
         }
-
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
